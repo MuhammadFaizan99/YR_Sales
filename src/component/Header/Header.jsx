782fbfbf979006d7fae136bac30a1791aef4cfeb
Fazi <faf2001f@gmail.com>
2024-03-10 02:43:12 +0500

    first commit

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
   return (
     <div className={HeaderStyles.header}>
       <div className={HeaderStyles.logo}>
-        <img src="../../../assets/logo.png" alt="Logo" />
+        <img src="../../../public/assets/yr_sales.png" alt="Logo" />
       </div>
       <div className={HeaderStyles.navButtons}>
         {windowWidth <= 700 ? (
